780ac8c153da9a8e40c96c38358094685c7524e0
weselyong <wesley.ong@grindr.com>
2020-03-03 15:37:23 +0800

    setup github

---
+++ app/src/main/java/com/grindr/android/framerate/ui/anim/ViewAnimFragment.kt
@@ -0,0 +1,19 @@
+package com.grindr.android.framerate.ui.anim
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.grindr.android.framerate.R
+
+class ViewAnimFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_notifications, container, false)
+    }
+}
---
+++ app/src/main/java/com/grindr/android/framerate/ui/gif/GifSampleFragment.kt
@@ -0,0 +1,28 @@
+package com.grindr.android.framerate.ui.gif
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
+import com.grindr.android.framerate.R
+import kotlinx.android.synthetic.main.fragment_home.*
+
+class GifSampleFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Glide.with(this)
+            .load(R.raw.sample_gif)
+            .into(ivGif)
+    }
+}
